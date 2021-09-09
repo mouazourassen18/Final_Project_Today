92a272f437857462e8a27ef70e72ea5d02755bb9
mouazourassen18 <54610297+mouazourassen18@users.noreply.github.com>
2021-09-09 09:51:01 -0400

    ccc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  // here you can change note  to newNote that was passed from
+  // onAdd
+  function addNode(note) {
+    setNotes((prev) => {
+      return [...prev, note];
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNode} />
       <Note key={1} title="Note title" content="Note content" />
+      {notes.map((eachtem) => {
+        return <Note key={1} title={eachtem.title} content={eachtem.content} />;
+      })}
+      {/* // now we add an array in the area   as Note its nuits */}
+
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+// we will need a function to pass this node to the App
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
 
-function CreateArea() {
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+  // event down, is object passed from add button
+  function submitNode(event) {
+    // we are passing a method that will take a node to App
+    props.onAdd(note);
+    event.preventDefault();
+  }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+        />
+        <button onClick={submitNode}>Add</button>
       </form>
     </div>
   );
